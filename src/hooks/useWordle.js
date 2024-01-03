47ceff105f941676e247d37154503d28cb7aa40a
subradeep <subradeep.chowdhury.sc.sc@gmail.com>
2024-01-03 23:54:26 +0530

    checking guesses

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -3,19 +3,70 @@ import { useState } from "react"
 const useWordle = (solution) => {
     const [turn,setTurn] = useState(0)
     const [currentGuess,setCurrentGuess] = useState('')
-    const [guesses, setGuesses] = useState([])
+    const [guesses, setGuesses] = useState([...Array(6)])
     const [history, setHistory] = useState([])
     const [isCorrect, setIsCorrect] = useState(false)
 
     const formatGuess = () => {
+        let solutionArray = [...solution]
+        let formattedGuess = [...currentGuess].map((l) => {
+            return {key : l, color: 'grey'}
+        })
 
+        // find any green letter
+        formattedGuess.forEach((l, i) => {
+            if (solutionArray[i] == l.key) {
+                formattedGuess[i].color = 'green'
+                solutionArray[i] = null
+            }
+        })
+
+        // find any yellow letter
+        formattedGuess.forEach((l, i) => {
+            if (solutionArray.includes(l.key) && l.color !== 'green') {
+                formattedGuess[i].color = 'yellow'
+                solutionArray[solutionArray.indexOf(l.key)] = null
+            }
+        })
+        return formattedGuess
     }
 
-    const addNewGuess = () => {
-        
+    const addNewGuess = (formattedGuess) => {
+        if (currentGuess === solution) {
+            setIsCorrect(true)
+        }
+        setGuesses((prevGuesses) => {
+            let newGuesses = [...prevGuesses]
+            newGuesses[turn] = formattedGuess
+            return newGuesses
+        })
+        setHistory((prevHistory) => {
+            return [...prevHistory, currentGuess]
+        })
+        setTurn((prevTurn) => {
+            return prevTurn + 1
+        })
+        setCurrentGuess('')
     }
 
     const handleKeyUp = ({key}) => {
+
+        if (key === 'Enter') {
+            if (turn > 5){
+                console.log('You used all your guesses')
+                return
+            }
+            if (history.includes(currentGuess)) {
+                console.log('You already tried that word')
+                return
+            }
+            if (currentGuess.length !== 5) {
+                console.log('Word must be 5 chars long')
+                return
+            }
+            const formatted = formatGuess()
+            addNewGuess(formatted)
+        }
 
         if (key === 'Backspace') {
             setCurrentGuess((prev) => {
