5fbf319f89fd0733d6e17b96ca49d3c3c1845b5f
subradeep <subradeep.chowdhury.sc.sc@gmail.com>
2024-01-05 23:30:32 +0530

    Current Guess

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,12 +1,15 @@
 import React from 'react'
-
-// components
 import Row from './Row'
 
-export default function Grid({ guesses, currentGuess, turn }) {
+// components
+
+export default function Grid({ currentGuess ,guesses, turn }) {
   return (
     <div>
       {guesses.map((g, i) => {
+        if (turn === i) {
+          return <Row key={i} currentGuess={currentGuess} />
+        }
         return <Row key={i} guess={g} /> 
       })}
     </div>
