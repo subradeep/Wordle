a19cd8b939303e864492bd399d6d26ab61978411
subradeep <subradeep.chowdhury.sc.sc@gmail.com>
2024-01-06 21:38:27 +0530

    Styling

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import useWordle from '../hooks/useWordle'
 
 import Grid from './Grid'
+import Keypad from './Keypad'
 
 export default function Wordle({solution}) {
     const { currentGuess, handleKeyUp, guesses, isCorrect, turn } = useWordle(solution)
@@ -21,6 +22,7 @@ export default function Wordle({solution}) {
       <div>solution - {solution}</div>
       <div>Current Guess - {currentGuess}</div>
       <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
+      <Keypad />
     </div>
   )
 }
